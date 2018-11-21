c4a5736ce9bfac69727bad16d905be6d7d9f5cf1
Brandon Gealy <brandon.gealy@gmail.com>
2018-11-20 20:30:41 -0800

    Update binary_tree.js

--- binary_tree.js
+++ binary_tree.js
@@ -1,51 +1,29 @@
-// Binary search is a technique for very rapidly searching a sorted collection by cutting the search space in half at every pass.
-
-// Given a sorted array, such as this:
-// [1, 3, 16, 22, 31, 33, 34]
-
-// You can search for the value 31, as follows:
-
-// 1) Pick the midpoint: 22.
-// 2) The value is higher than 22, so now consider only the right half of the previous array:
-// [...31, 33, 34]
-// 3) Pick the midpoint: 33
-// 4) The value is lower than 33, so now consider only the left half of the previous array:
-// [...31...]
-// 5) Pick the midpoint: 31
-// 6) You've hit the value.
-// 7) Return the index of the value.
-
-// Notes:
-// * If you don't find the value, you can return null.
-// * If at any point you calculate the index of the midpoint and get a fractional number, just round it down ("floor" it).
-
-// // if array is not in ascending order:
-// // let numbers = [100, 200, 5, 13, 35];
-// // numbers.sort(function(a, b) {
-// //   return a - b;
-// // });
-// // console.log(numbers);
-
 function search(array, value) {
+    // set a placeholder for the low, high and midpoint(s) and a starting position
     let lowIndex = 0;
     let highIndex = array.length - 1;
     let midpoint = 0;
     let currentElem = 0;
+    // iterate through the array and set the midpoint and starting position at this halfway point
     for (let i = 0; i < array.length; i++) {
         midpoint = Math.floor((lowIndex + highIndex) / 2);
         currentElem = array[midpoint];
+        // if where we're at is less than our target value then set the low point above and search to the top
         if (currentElem < value) {
             lowIndex = midpoint + 1;
+            // else if the target value is less set the high point below and search to the bottom
         } else if (currentElem > value) {
             highIndex = midpoint - 1;
+            // if neither of those then it must be the midpoint
         } else {
             return midpoint;
         }
     }
+    // all else fails not there
     return null;
 }
 
-
+// test to see if what we got is what we accepted
 function assertEquals(actual, expected, testName){
     if (actual === expected) {
         console.log(`PASSED – ${testName}: Found ${value} at index ${output}`);
